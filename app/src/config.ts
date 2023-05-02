cf86659c5cbe570a5907cf54c65f5a776411cbcf
EasonApolo <787817128@qq.com>
2023-05-03 04:06:41 +0800

    3.6.3.1 修改ip等

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,3 +1,3 @@
-export const env = location.href.includes('forvera.me') ? 'online' : 'local'
+export const env = location.href.includes('easons') ? 'online' : 'local'
 
 export const ip = env === 'online' ? `http://106.54.172.20:3000/` : `http://${location.hostname}:3000/`
